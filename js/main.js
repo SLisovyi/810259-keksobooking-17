4909b86bb898c4294ebf873d9839bc39009cd102
Sergii Lisovyi <slisoviy@gmail.com>
2019-06-23 21:07:08 -0600

    module4 начало

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
 // function случайное число
-function randomInteger(min, max) {
+var randomInteger = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   rand = Math.floor(rand);
   return rand;
-}
+};
 
-// убираем faded class for Map
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
 
 // блок с пинами пользователей
 var pinButtonsList = document.querySelectorAll('.map__pins');
@@ -51,14 +48,65 @@ var getUsersData = function () {
 };
 
 // генерируем Pins из массива данных userAds
-for (var i = 0; i < getUsersData().length; i++) {
-  var pinElement = similarPinTemplate.cloneNode(true);
+var createPins = function () {
 
-  pinElement.style.left = getUsersData()[i].location.x + 'px';
-  pinElement.style.top = getUsersData()[i].location.y + 'px';
+  for (var i = 0; i < getUsersData().length; i++) {
+    var pinElement = similarPinTemplate.cloneNode(true);
 
-  pinElement.querySelector('img').src = getUsersData()[i].author.avatar;
-  pinElement.querySelector('img').alt = getUsersData()[i].author.title;
+    pinElement.style.left = getUsersData()[i].location.x + 'px';
+    pinElement.style.top = getUsersData()[i].location.y + 'px';
 
-  pinButtonsList[0].appendChild(pinElement);
-}
+    pinElement.querySelector('img').src = getUsersData()[i].author.avatar;
+    pinElement.querySelector('img').alt = getUsersData()[i].author.title;
+
+    pinButtonsList[0].appendChild(pinElement);
+  }
+};
+
+// активация карты при нажатии на  метку
+var mainPin = document.querySelector('.map__pin--main');
+var map = document.querySelector('.map');
+var fieldForm = document.querySelector('.ad-form');
+
+var inputs = fieldForm.querySelectorAll('input');
+var textarea = fieldForm.querySelectorAll('textarea');
+var select = document.querySelectorAll('select');
+
+// добавляем тег disablet на формы и поля
+var setInputDisabled = function (onOff) {
+
+  inputs.forEach(function (element) {
+    element.setAttribute('disabled', onOff);
+  });
+  textarea.forEach(function (element) {
+    element.setAttribute('disabled', onOff);
+  });
+  select.forEach(function (element) {
+    element.setAttribute('disabled', onOff);
+  });
+};
+setInputDisabled();
+
+// убираем тег disablet с форм и полей
+var removeInputDisabled = function (onOff) {
+
+  inputs.forEach(function (element) {
+    element.removeAttribute('disabled', onOff);
+  });
+  textarea.forEach(function (element) {
+    element.removeAttribute('disabled', onOff);
+  });
+  select.forEach(function (element) {
+    element.removeAttribute('disabled', onOff);
+  });
+};
+
+// действия при клике на mapPin
+mainPin.addEventListener('click', function () {
+  map.classList.remove('map--faded');
+  fieldForm.classList.remove('ad-form--disabled');
+
+  createPins();
+
+  removeInputDisabled(true);
+});
